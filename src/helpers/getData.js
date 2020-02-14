216cf0f99060b4125f50a32a605500a9d4c8dd83
Andrian Bitca <andrian.bitca@bigdatafed.com>
2020-02-14 16:58:55 +0200

    get anime data

--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -1,8 +1,8 @@
 export async function getData() {
   try {
-    const data = await this.fetch("https://api.jikan.moe/v3/top/anime/1/tv");
+    const data = await fetch("https://api.jikan.moe/v3/top/anime/1/tv");
     const jsonData = await data.json();
-    return jsonData;
+    return jsonData.top;
   } catch (err) {
     console.log(err);
   }
